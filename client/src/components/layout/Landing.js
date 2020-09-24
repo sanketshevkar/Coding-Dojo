5141eaa3f0c99362ed154ce8414d479fd0952095
User <shevkar.sanket@gmail.com>
2020-09-24 09:25:00 +0530

    Design Updated

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,13 +7,10 @@ class Landing extends Component {
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
+              <b>Create Read Update Delete</b> Your Daily Post-IT Notes
             </h4>
             <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+              Prioritise your notes according to your needs using drag and drop
             </p>
             <br />
             <div className="col s6">
@@ -37,7 +34,7 @@ class Landing extends Component {
                   borderRadius: "3px",
                   letterSpacing: "1.5px"
                 }}
-                className="btn btn-large btn-flat waves-effect white black-text"
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
               >
                 Log In
               </Link>
